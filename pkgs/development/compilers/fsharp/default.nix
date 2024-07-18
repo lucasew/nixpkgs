017c9333b245584979fb968c60fc5a2b92e24f28
lucasew <lucas59356@gmail.com>
2024-07-18 11:30:34 -0300

    fsharp: checkpoint
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- pkgs/development/compilers/fsharp/default.nix
+++ pkgs/development/compilers/fsharp/default.nix
@@ -1,8 +1,8 @@
 # Temporarily avoid dependency on dotnetbuildhelpers to avoid rebuilding many times while working on it
 
-{ lib, stdenv, fetchFromGitHub, mono, pkg-config, dotnetbuildhelpers, autoconf, automake, which, gitUpdater }:
+{ lib, stdenv, fetchFromGitHub, mono, pkg-config, dotnetbuildhelpers, autoconf, automake, which, gitUpdater, buildDotnetModule, dotnetCorePackages }:
 
-stdenv.mkDerivation rec {
+buildDotnetModule rec {
   pname = "fsharp";
   version = "12.8.0";
 
@@ -13,28 +13,35 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-vpntjsh9iIziJgRmXHfxDDC/C9sfU2ssuFdnBh2qgIw=";
   };
 
-  nativeBuildInputs = [ pkg-config autoconf automake ];
-  buildInputs = [ mono dotnetbuildhelpers which ];
+  projectFile = "FSharp.sln";
 
-  configurePhase = ''
-    sed -i '988d' src/FSharpSource.targets
-    substituteInPlace ./autogen.sh --replace "/usr/bin/env sh" "${stdenv.shell}"
-    ./autogen.sh --prefix $out
-  '';
+  dotnet-sdk = dotnetCorePackages.sdk_8_0;
+  dotnet-runtime = dotnetCorePackages.runtime_8_0;
 
-  # Make sure the executables use the right mono binary,
-  # and set up some symlinks for backwards compatibility.
-  postInstall = ''
-    substituteInPlace $out/bin/fsharpc --replace " mono " " ${mono}/bin/mono "
-    substituteInPlace $out/bin/fsharpi --replace " mono " " ${mono}/bin/mono "
-    substituteInPlace $out/bin/fsharpiAnyCpu --replace " mono " " ${mono}/bin/mono "
-    ln -s $out/bin/fsharpc $out/bin/fsc
-    ln -s $out/bin/fsharpi $out/bin/fsi
-    for dll in "$out/lib/mono/4.5"/FSharp*.dll
-    do
-      create-pkg-config-for-dll.sh "$out/lib/pkgconfig" "$dll"
-    done
-  '';
+  nugetDeps = ./deps.nix;
+
+  # nativeBuildInputs = [ pkg-config autoconf automake ];
+  # buildInputs = [ mono dotnetbuildhelpers which ];
+
+  # configurePhase = ''
+  #   sed -i '988d' src/FSharpSource.targets
+  #   substituteInPlace ./autogen.sh --replace "/usr/bin/env sh" "${stdenv.shell}"
+  #   ./autogen.sh --prefix $out
+  # '';
+
+  # # Make sure the executables use the right mono binary,
+  # # and set up some symlinks for backwards compatibility.
+  # postInstall = ''
+  #   substituteInPlace $out/bin/fsharpc --replace " mono " " ${mono}/bin/mono "
+  #   substituteInPlace $out/bin/fsharpi --replace " mono " " ${mono}/bin/mono "
+  #   substituteInPlace $out/bin/fsharpiAnyCpu --replace " mono " " ${mono}/bin/mono "
+  #   ln -s $out/bin/fsharpc $out/bin/fsc
+  #   ln -s $out/bin/fsharpi $out/bin/fsi
+  #   for dll in "$out/lib/mono/4.5"/FSharp*.dll
+  #   do
+  #     create-pkg-config-for-dll.sh "$out/lib/pkgconfig" "$dll"
+  #   done
+  # '';
 
   # To fix this error when running:
   # The file "/nix/store/path/whatever.exe" is an not a valid CIL image
