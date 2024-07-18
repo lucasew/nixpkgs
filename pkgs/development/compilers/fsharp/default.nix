7df3089fd5ae20920da0b6fb1a4da773694cc042
lucasew <lucas59356@gmail.com>
2024-07-18 14:48:07 -0300

    fsharp: checkpoint
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- pkgs/development/compilers/fsharp/default.nix
+++ pkgs/development/compilers/fsharp/default.nix
@@ -1,6 +1,14 @@
 # Temporarily avoid dependency on dotnetbuildhelpers to avoid rebuilding many times while working on it
 
-{ lib, stdenv, fetchFromGitHub, mono, pkg-config, dotnetbuildhelpers, autoconf, automake, which, gitUpdater, buildDotnetModule, dotnetCorePackages }:
+{ lib, stdenv, fetchFromGitHub, mono, pkg-config, dotnetbuildhelpers, autoconf, automake, which, gitUpdater, buildDotnetModule, dotnetCorePackages, writeShellScriptBin, callPackage }:
+
+let
+  noop = writeShellScriptBin "noop" ''
+    echo noop: "$@" >&2
+  '';
+
+  fsharpBootstrap = callPackage ./bootstrap.nix {};
+in
 
 buildDotnetModule rec {
   pname = "fsharp";
@@ -20,8 +28,37 @@ buildDotnetModule rec {
 
   nugetDeps = ./deps.nix;
 
-  # nativeBuildInputs = [ pkg-config autoconf automake ];
-  # buildInputs = [ mono dotnetbuildhelpers which ];
+  postPatch = ''
+    patchShebangs ./build.sh ./eng/*.sh
+    mkdir -p .dotnet
+    echo true > .dotnet/dotnet-install.sh
+    ln -s $(which dotnet) .dotnet/dotnet
+    # export "_InitializeDotNetCli=$(realpath $(which dotnet)/../..)"
+    # env
+    # exit 1
+
+    # substituteInPlace eng/build.sh \
+    #   --replace-warn '. "$scriptroot/common/tools.sh"' ""
+
+    # substituteInPlace eng/common/tools.sh \
+    #   --replace-warn wget ${lib.getExe noop}
+  '';
+
+  preBuild = ''
+    for proj in fslex fsyacc; do
+      find -type f -name $proj.dll
+      dotnet build buildtools/$proj/$proj.fsproj
+      mkdir -p artifacts/Bootstrap/$proj
+      ln -s artifacts/obj/$proj/**/$proj.dll artifacts/Bootstrap/$proj
+    done
+    echo chegou aqui
+    ./build.sh --bootstrap --restore
+    # ./build.sh
+    # dotnet publish $(pwd)/proto.proj /restore /p:Configuration=Proto /p:ArcadeBuildFromSource=true
+  '';
+
+  nativeBuildInputs = [ pkg-config autoconf automake noop fsharpBootstrap ];
+  buildInputs = [ mono dotnetbuildhelpers which ];
 
   # configurePhase = ''
   #   sed -i '988d' src/FSharpSource.targets
