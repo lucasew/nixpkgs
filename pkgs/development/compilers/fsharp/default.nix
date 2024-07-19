00da7ebcbbd88a34833d7374ffc0f52a4f28f26e
lucasew <lucas59356@gmail.com>
2024-07-18 22:51:04 -0300

    fsharp: checkpoint
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- pkgs/development/compilers/fsharp/default.nix
+++ pkgs/development/compilers/fsharp/default.nix
@@ -1,13 +1,12 @@
 # Temporarily avoid dependency on dotnetbuildhelpers to avoid rebuilding many times while working on it
 
-{ lib, stdenv, fetchFromGitHub, mono, pkg-config, dotnetbuildhelpers, autoconf, automake, which, gitUpdater, buildDotnetModule, dotnetCorePackages, writeShellScriptBin, callPackage }:
+{ lib, stdenv, gitMinimal, fetchFromGitHub, mono, pkg-config, dotnetbuildhelpers, autoconf, automake, which, gitUpdater, buildDotnetModule, dotnetCorePackages, writeShellScriptBin, callPackage, dos2unix }:
 
 let
   noop = writeShellScriptBin "noop" ''
     echo noop: "$@" >&2
   '';
 
-  fsharpBootstrap = callPackage ./bootstrap.nix {};
 in
 
 buildDotnetModule rec {
@@ -18,7 +17,10 @@ buildDotnetModule rec {
     owner = "dotnet";
     repo = "fsharp";
     rev = "v${version}";
-    sha256 = "sha256-vpntjsh9iIziJgRmXHfxDDC/C9sfU2ssuFdnBh2qgIw=";
+    fetchSubmodules = true;
+    deepClone = true;
+    leaveDotGit = true;
+    hash = "sha256-dSucTf9FrH0Om4zA85YcMSrWxFevrdGwgQKm68cBdRU=";
   };
 
   projectFile = "FSharp.sln";
@@ -29,35 +31,77 @@ buildDotnetModule rec {
   nugetDeps = ./deps.nix;
 
   postPatch = ''
+    # dos2unix ../**/*.bsl
+    # dos2unix tests/**/*.bsl $HOME/.nuget/**/*.bsl
     patchShebangs ./build.sh ./eng/*.sh
-    mkdir -p .dotnet
-    echo true > .dotnet/dotnet-install.sh
-    ln -s $(which dotnet) .dotnet/dotnet
+    # mkdir -p .dotnet
+    # echo true > .dotnet/dotnet-install.sh
+    # ln -s $(which dotnet) .dotnet/dotnet
+
+    # mkdir -p artifacts/source-build/self/src/.dotnet
+    # ln -s $(which dotnet) artifacts/source-build/self/src/.dotnet/dotnet
+    # echo true > artifacts/source-build/self/src/.dotnet/dotnet-install.sh
+
+    # git config --global core.autocrlf true # this is causing some errors
+    # git config --global advice.detachedHead false
+
     # export "_InitializeDotNetCli=$(realpath $(which dotnet)/../..)"
-    # env
+    echo env
+    env
     # exit 1
 
     # substituteInPlace eng/build.sh \
     #   --replace-warn '. "$scriptroot/common/tools.sh"' ""
 
-    # substituteInPlace eng/common/tools.sh \
-    #   --replace-warn wget ${lib.getExe noop}
+    substituteInPlace eng/common/tools.sh \
+      --replace-warn wget ${lib.getExe noop}
   '';
 
   preBuild = ''
-    for proj in fslex fsyacc; do
-      find -type f -name $proj.dll
-      dotnet build buildtools/$proj/$proj.fsproj
-      mkdir -p artifacts/Bootstrap/$proj
-      ln -s artifacts/obj/$proj/**/$proj.dll artifacts/Bootstrap/$proj
-    done
+    # for proj in fslex fsyacc; do
+    #   find -type f -name $proj.dll
+    #   dotnet build buildtools/$proj/$proj.fsproj
+    #   mkdir -p artifacts/Bootstrap/$proj
+    #   ln -s artifacts/obj/$proj/**/$proj.dll artifacts/Bootstrap/$proj
+    # done
+    git add -A
+
+    mkdir -p .dotnet
+    ln -s $(which dotnet) .dotnet/dotnet
+    echo true > .dotnet/dotnet-install.sh
+
+    {
+      echo '('
+      echo 'cd $destDir'
+      echo 'echo chegou em $destDir'
+      echo 'pwd'
+      echo 'mkdir -p .dotnet'
+      echo 'ln -s $(which dotnet) .dotnet/dotnet'
+      echo 'echo true > .dotnet/dotnet-install.sh'
+      echo ')'
+    } >> $HOME/.nuget/packages/microsoft.dotnet.arcade.sdk/8.0.0-beta.23463.1/tools/SourceBuild/git-clone-to-dir.sh
+
+    patchShebangs $HOME/.nuget/packages/microsoft.dotnet.arcade.sdk/8.0.0-beta.23463.1/tools/SourceBuild/git-clone-to-dir.sh
+    cat $HOME/.nuget/packages/microsoft.dotnet.arcade.sdk/8.0.0-beta.23463.1/tools/SourceBuild/git-clone-to-dir.sh
+    # ln -sf ${lib.getExe noop} $HOME/.nuget/packages/microsoft.dotnet.arcade.sdk/8.0.0-beta.23463.1/tools/SourceBuild/git-clone-to-dir.sh
+    # mkdir -p artifacts/source-build/self
+    # ln -s $(realpath .) artifacts/source-build/self/src
+
     echo chegou aqui
-    ./build.sh --bootstrap --restore
+    ./build.sh --restore /p:ArcadeBuildFromSource=true --bootstrap 
     # ./build.sh
     # dotnet publish $(pwd)/proto.proj /restore /p:Configuration=Proto /p:ArcadeBuildFromSource=true
   '';
 
-  nativeBuildInputs = [ pkg-config autoconf automake noop fsharpBootstrap ];
+  nativeBuildInputs = [
+    pkg-config
+    # autoconf
+    # automake
+    noop
+    gitMinimal
+    dos2unix
+    # fsharpBootstrap
+  ];
   buildInputs = [ mono dotnetbuildhelpers which ];
 
   # configurePhase = ''
@@ -84,9 +128,11 @@ buildDotnetModule rec {
   # The file "/nix/store/path/whatever.exe" is an not a valid CIL image
   dontStrip = true;
 
-  passthru.updateScript = gitUpdater {
-    rev-prefix = "v";
-    ignoredVersions = ["beta"];
+  passthru = {
+    updateScript = gitUpdater {
+      rev-prefix = "v";
+      ignoredVersions = ["beta"];
+    };
   };
 
   meta = {
